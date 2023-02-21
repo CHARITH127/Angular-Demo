b3e56d54af16fd788c1cc872ce85688829dc98f3
CHARITH127 <charithrullz993@gmail.com>
2023-02-21 15:08:30 +0530

    add two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,4 @@ export class AppComponent {
   price = 100;
   discount = 30;
 
-  updatePrice(price: string) {
-    this.price = +price;
-  }
-
-  updateDiscount(discount: string) {
-    this.discount = +discount;
-  }
 }
