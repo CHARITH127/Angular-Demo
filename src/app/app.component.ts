ec99f49443c17cde9a45f2a1c866346089990246
CHARITH127 <charithrullz993@gmail.com>
2023-02-21 13:11:47 +0530

    Start a angular demo project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  price = 100;
+  discount = 30;
+}
