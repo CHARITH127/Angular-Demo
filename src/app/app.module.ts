b186867c21ad9f25681d41bec69d2c9fba34987d
CHARITH127 <charithrullz993@gmail.com>
2023-02-21 15:58:07 +0530

    add custom created pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import { PercentagePipe } from './pipe/percentage.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    PercentagePipe,
   ],
   imports: [
     BrowserModule,
