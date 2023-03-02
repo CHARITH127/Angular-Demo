80ab50f255158e86cf65f07777e8ff4a974f0a73
CHARITH127 <charithrullz993@gmail.com>
2023-03-02 16:01:38 +0530

    add ng switchCase

--- src/app/structural/structural.component.ts
+++ src/app/structural/structural.component.ts
@@ -7,8 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class StructuralComponent implements OnInit {
 
-  showPara =true;
-  students : Array<{id: number, name: string}> =[];
+  showPara = true;
+  students: Array<{ id: number, name: string , rating : number }> = [];
 
   enablePara() {
     this.showPara = !this.showPara
@@ -18,9 +18,22 @@ export class StructuralComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.students.push({ id: 1, name: 'Manoj' })
-    this.students.push({ id: 2, name: 'Saman' })
-    this.students.push({ id: 3, name: 'Kumara' })
-    this.students.push({ id: 4, name: 'Aruna' })
+    this.students.push({id: 1, name: 'Manoj' , rating : 5})
+    this.students.push({id: 2, name: 'Saman' , rating : 4})
+    this.students.push({id: 3, name: 'Kumara' , rating : 3})
+    this.students.push({id: 4, name: 'Aruna' , rating : 5})
+  }
+
+  reloadStudents() {
+    this.students = []
+    this.students.push({id: 1, name: 'Manoj' , rating : 3})
+    this.students.push({id: 2, name: 'Kasun' , rating : 5})
+    this.students.push({id: 3, name: 'Sunil' , rating : 3})
+    this.students.push({id: 4, name: 'Chethana' , rating : 4})
+  }
+
+
+  byId(index: number, s: { id: number, name: string }) {
+   return s.id
   }
 }
